efafd6de4e83de4271c7ed26bf0a95409bc84724
cutkoskys <cutkoskys@mu-048037.dhcp.missouri.edu>
2025-06-24 11:26:11 -0500

    to qiskit circuit function

--- rival_methods.py
+++ rival_methods.py
@@ -9,6 +9,18 @@ Created on Thu Jun  5 11:45:36 2025
 import qiskit_aer
 import numpy as np
 
+
+def to_qiskit_circuit(circuit, readout_qubits):
+    qiskit_circuit = qiskit.circuit.QuantumCircuit(num_qubits,len(readout_qubits))
+    for name, qubits, params in circuit:
+        if name == 'rz':
+            qiskit_circuit.rz(params[0],qubits[0])
+        elif name == 'cz':
+            qiskit_circuit.cz(qubits[0], qubits[1])
+        else:
+            getattr(qiskit_circuit,name)(qubits[0])
+    [qiskit_circuit.measure(q, i) for i, q in enumerate(readout_qubits)]
+
 class Qiskit_Aer:
     def __init__(self, backend = None, properties = None):
         if properties is None:
